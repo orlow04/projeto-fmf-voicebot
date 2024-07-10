126fac310b60d996d79d81ec4f3057b1823be72d
José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
2024-07-09 23:05:25 -0300

    organize

--- chatgui.py
+++ chatgui.py
@@ -1,17 +1,19 @@
-
 import nltk
 from nltk.stem import WordNetLemmatizer
+
 lemmatizer = WordNetLemmatizer()
 import pickle
-import numpy as np
 
-from keras import models 
-model = models.load_model('chatbot_model.h5')
+import numpy as np
+from keras import models
+
+model = models.load_model("chatbot_model.h5")
 import json
 import random
-intents = json.loads(open('intents.json').read())
-words = pickle.load(open('words.pkl','rb'))
-classes = pickle.load(open('classes.pkl','rb'))
+
+intents = json.loads(open("intents.json").read())
+words = pickle.load(open("words.pkl", "rb"))
+classes = pickle.load(open("classes.pkl", "rb"))
 
 
 def clean_up_sentence(sentence):
@@ -19,28 +21,31 @@ def clean_up_sentence(sentence):
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
+
 # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
 
 def bow(sentence, words, show_details=True):
     # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
     # bag of words - matrix of N words, vocabulary matrix
-    bag = [0]*len(words)
+    bag = [0] * len(words)
     for s in sentence_words:
-        for i,w in enumerate(words):
+        for i, w in enumerate(words):
             if w == s:
                 # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
-                    print ("found in bag: %s" % w)
-    return(np.array(bag))
+                    print("found in bag: %s" % w)
+    return np.array(bag)
+
 
 def predict_class(sentence, model):
     # filter out predictions below a threshold
-    p = bow(sentence, words,show_details=False)
+    p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
@@ -48,14 +53,16 @@ def predict_class(sentence, model):
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
 
+
 def getResponse(ints, intents_json):
-    tag = ints[0]['intent']
-    list_of_intents = intents_json['intents']
+    tag = ints[0]["intent"]
+    list_of_intents = intents_json["intents"]
     for i in list_of_intents:
-        if(i['tag']== tag):
-            result = random.choice(i['responses'])
+        if i["tag"] == tag:
+            result = random.choice(i["responses"])
             break
     return result
+
 
 def chatbot_response(msg):
     ints = predict_class(msg, model)
@@ -63,22 +70,22 @@ def chatbot_response(msg):
     return res
 
 
-#Creating GUI with tkinter
+# Creating GUI with tkinter
 import tkinter
 from tkinter import *
 
 
 def send():
-    msg = EntryBox.get("1.0",'end-1c').strip()
-    EntryBox.delete("0.0",END)
+    msg = EntryBox.get("1.0", "end-1c").strip()
+    EntryBox.delete("0.0", END)
 
-    if msg != '':
+    if msg != "":
         ChatLog.config(state=NORMAL)
-        ChatLog.insert(END, "You: " + msg + '\n\n')
-        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+        ChatLog.insert(END, "You: " + msg + "\n\n")
+        ChatLog.config(foreground="#442265", font=("Verdana", 12))
 
         res = chatbot_response(msg)
-        ChatLog.insert(END, "Bot: " + res + '\n\n')
+        ChatLog.insert(END, "Bot: " + res + "\n\n")
 
         ChatLog.config(state=DISABLED)
         ChatLog.yview(END)
@@ -89,28 +96,44 @@ base.title("Hello")
 base.geometry("400x500")
 base.resizable(width=FALSE, height=FALSE)
 
-#Create Chat window
-ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+# Create Chat window
+ChatLog = Text(
+    base,
+    bd=0,
+    bg="white",
+    height="8",
+    width="50",
+    font="Arial",
+)
 
 ChatLog.config(state=DISABLED)
 
-#Bind scrollbar to Chat window
+# Bind scrollbar to Chat window
 scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
-ChatLog['yscrollcommand'] = scrollbar.set
+ChatLog["yscrollcommand"] = scrollbar.set
 
-#Create Button to send message
-SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
-                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
-                    command= send )
+# Create Button to send message
+SendButton = Button(
+    base,
+    font=("Verdana", 12, "bold"),
+    text="Send",
+    width="12",
+    height=5,
+    bd=0,
+    bg="#32de97",
+    activebackground="#3c9d9b",
+    fg="#ffffff",
+    command=send,
+)
 
-#Create the box to enter message
-EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
-#EntryBox.bind("<Return>", send)
+# Create the box to enter message
+EntryBox = Text(base, bd=0, bg="white", width="29", height="5", font="Arial")
+# EntryBox.bind("<Return>", send)
 
 
-#Place all components on the screen
-scrollbar.place(x=376,y=6, height=386)
-ChatLog.place(x=6,y=6, height=386, width=370)
+# Place all components on the screen
+scrollbar.place(x=376, y=6, height=386)
+ChatLog.place(x=6, y=6, height=386, width=370)
 EntryBox.place(x=128, y=401, height=90, width=265)
 SendButton.place(x=6, y=401, height=90)
 
--- train_chatbot.py
+++ train_chatbot.py
@@ -1,54 +1,55 @@
 import nltk
-nltk.download('punkt')
-nltk.download('wordnet')
+
+nltk.download("punkt")
+nltk.download("wordnet")
 from nltk.stem import WordNetLemmatizer
+
 lemmatizer = WordNetLemmatizer()
 import json
 import pickle
-
-import numpy as np
-import keras
-from keras import layers
-from keras import optimizers
 import random
 
-words=[]
+import keras
+import numpy as np
+from keras import layers, optimizers
+
+words = []
 classes = []
 documents = []
-ignore_words = ['?', '!', ',']
-data_file = open('intents.json').read()
+ignore_words = ["?", "!", ","]
+data_file = open("intents.json").read()
 intents = json.loads(data_file)
 
 
-for intent in intents['intents']:
-    for pattern in intent['patterns']:
+for intent in intents["intents"]:
+    for pattern in intent["patterns"]:
 
         # take each word and tokenize it
         w = nltk.word_tokenize(pattern)
         words.extend(w)
         # adding documents
-        documents.append((w, intent['tag']))
+        documents.append((w, intent["tag"]))
 
         # adding classes to our class list
-        if intent['tag'] not in classes:
-            classes.append(intent['tag'])
+        if intent["tag"] not in classes:
+            classes.append(intent["tag"])
 
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
 
 classes = sorted(list(set(classes)))
 
-print (len(documents), "documents")
+print(len(documents), "documents")
 
-print (len(classes), "classes", classes)
+print(len(classes), "classes", classes)
 
-print (len(words), "unique lemmatized words", words)
+print(len(words), "unique lemmatized words", words)
 words.remove("'s")
 
 
-pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(words, open("words.pkl", "wb"))
 print(words.shape())
-pickle.dump(classes,open('classes.pkl','wb'))
+pickle.dump(classes, open("classes.pkl", "wb"))
 
 # initializing training data
 training = []
@@ -73,26 +74,28 @@ for doc in documents:
 random.shuffle(training)
 training = np.array(training)
 # create train and test lists. X - patterns, Y - intents
-train_x = list(training[:,0])
-train_y = list(training[:,1])
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
 print("Training data created")
 
 
 # Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
 # equal to number of intents to predict output intent with softmax
 model = keras.Sequential()
-model.add(layers.Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(layers.Dense(128, input_shape=(len(train_x[0]),), activation="relu"))
 model.add(layers.Dropout(0.5))
-model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(64, activation="relu"))
 model.add(layers.Dropout(0.5))
-model.add(layers.Dense(len(train_y[0]), activation='softmax'))
+model.add(layers.Dense(len(train_y[0]), activation="softmax"))
 
 # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
 sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
 
-#fitting and saving the model
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('chatbot_model.h5', hist)
+# fitting and saving the model
+hist = model.fit(
+    np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1
+)
+model.save("chatbot_model.h5", hist)
 
 print("model created")
