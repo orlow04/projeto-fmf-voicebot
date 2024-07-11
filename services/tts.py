96e57414db81b658d6dd63dad82b6d9171fd3a97
José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
2024-07-11 03:08:31 -0300

    updated

--- services/tts.py
+++ services/tts.py
@@ -3,30 +3,18 @@ import os
 from openai import OpenAI
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-
-def calculate_cost(text_string, model_id):
-    cost_tier = {
-        "tts-1": 0.015,
-        "tts-1-hd": 0.03,
-    }
-    cost_unit = cost_tier.get(model_id, None)
-    if cost_unit is None:
-        return None
-    return cost_unit * len(text_string) / 1000
-
-
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 text_input = """
-Esse é um teste de texto para ver se a API do OpenAI está funcionando corretamente. O texto é bem simples e não deve ter problemas.
+Olá, boa tarde! No Hospital BIAmigos, temos a Doutora Ana Souza, especialista em Pediatria.
+Infelizmente, na segunda-feira às 16 horas, a Doutora Ana Souza estará indisponível.
+No entanto, ela tem disponibilidade na segunda-feira das 08:00 às 09:00, das 09:00 às 10:00, das 14:00 às 15:00.
+Se precisar de ajuda para agendar uma consulta em um desses horários, por favor,
+forneça seu nome, CPF e telefone para podermos ajudá-lo da melhor forma. Estamos à disposição para auxiliá-lo no Hospital BIAmigos.
 """
-
 model = "tts-1"
-voice = "echo"
+voice = "shimmer"
 
 response = client.audio.speech.create(model=model, voice=voice, input=text_input)
 
-request_cost = calculate_cost(text_input, model)
-print(f"Request cost: ${request_cost:.3f}")
 response.write_to_file("./output/audio.mp3")
